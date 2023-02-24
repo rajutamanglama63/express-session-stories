f7daa1dbd38bd9c6fdd6d5e8fbbdc501e8fd67b8
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-02-24 19:09:10 +0545

    feat: user schema defined

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString();
+    delete returnObject._id;
+    delete returnObject.__v;
+
+    delete returnObject.password;
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
