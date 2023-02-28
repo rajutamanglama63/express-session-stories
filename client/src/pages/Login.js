a53a9b5decc7cc28fe1cccf79d1e07beb962f6ad
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-02-28 15:38:59 +0545

    feat: responsive ui of signup/signin created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="wrapper flex block-view">
-      <div className="container-md">
+      <div className="container-md container-tn">
         <div className="flex block-view region-sm">
           <h4 className="h6">Story Application</h4>
           <p className="one-font-size">Login to proceed further.</p>
