c990915e6a69a0428aae620b211166f6f3b9971a
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-03-01 21:22:22 +0545

    feat: hamburger menu added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import { Icon } from "@iconify/react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="shadow">
-      <div className="wrapper flex split-pair align-center flow-direction-tn">
+      <div className="wrapper flex split-pair align-center">
+        <Icon
+          icon="game-icons:hamburger-menu"
+          className="hamburger mob-hamburger"
+        />
         <div className="logo h2">Story</div>
-        <div className="flex split-pair align-center gap-2">
+        <div className="flex split-pair align-center gap-2 mob-nav-link">
           <NavLink to="/" className="pointer text-link font-sm">
             Home
           </NavLink>
           <NavLink to="/profile/:id" className="pointer text-link font-sm">
             Raju Lama
           </NavLink>
-          <button className="learn-btn ">Logout</button>
+          <button className="learn-btn">Logout</button>
         </div>
       </div>
     </div>
