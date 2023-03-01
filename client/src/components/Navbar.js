3b8e4c74e064f0475e350b0bdf6e8e51b8385cdd
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-03-01 13:38:09 +0545

    feat: trying to imply responsiveness in nav link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="shadow">
-      <div className="wrapper flex split-pair align-center ">
+      <div className="wrapper flex split-pair align-center flow-direction-tn">
         <div className="logo h2">Story</div>
         <div className="flex split-pair align-center gap-2">
-          <NavLink to="/" className="pointer text-link">
+          <NavLink to="/" className="pointer text-link font-sm">
             Home
           </NavLink>
-          <NavLink to="/profile/:id" className="pointer text-link">
+          <NavLink to="/profile/:id" className="pointer text-link font-sm">
             Raju Lama
           </NavLink>
           <button className="learn-btn ">Logout</button>
