3dc37ddcac270ea1b3f360c216df8960c8418a95
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-03-17 22:58:30 +0545

    feat: all done

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ function App() {
   const [open, setOpen] = useState(false);
 
   const user = utilityFunc.getUser();
-  console.log("user: ", user);
 
   const handleOpen = () => {
     setOpen(true);
