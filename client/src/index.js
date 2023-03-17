fc58e814f3e06df9fe6ca654ed2212a1f35d4328
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-03-17 23:00:56 +0545

    del: react-cookie deleted

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { CookiesProvider } from "react-cookie";
 
 import App from "./App";
 import store from "./store";
@@ -11,10 +10,8 @@ import "./styles/main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <CookiesProvider>
-      <Router>
-        <App />
-      </Router>
-    </CookiesProvider>
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
